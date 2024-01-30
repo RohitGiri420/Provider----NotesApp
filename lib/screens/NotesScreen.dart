f893c1d27d179b639e6fdcf3733a23e5b1e6b317
Rohit Giri <giri420rohit@gmail.com>
2024-01-30 10:21:20 +0530

    filter buttons is added

--- lib/screens/NotesScreen.dart
+++ lib/screens/NotesScreen.dart
@@ -9,6 +9,8 @@ class NotesScreen extends StatefulWidget {
 }
 
 class _NotesScreenState extends State<NotesScreen> {
+
+  UiHelper ui = UiHelper();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,10 +18,38 @@ class _NotesScreenState extends State<NotesScreen> {
       //BACKGROUND COLOR........................................................
       backgroundColor: Color.fromRGBO(8, 8, 8, 1),
 
+
       //APP BAR.................................................................
       appBar: AppBar(
         backgroundColor: Color.fromRGBO(8, 8, 8, 1),
-        title: UiHelper().CustomAppBar(),
+        title: ui.CustomAppBar(),
+      ),
+
+
+      //BODY....................................................................
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ui.CustomText(),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  ui.CustomFilterContainer("All (70)"),
+                  SizedBox(width: 20,),
+                  ui.CustomFilterContainer("Important"),
+                  SizedBox(width: 20,),
+                  ui.CustomFilterContainer("Bookmarked"),
+                  SizedBox(width: 20,),
+                  ui.CustomFilterContainer("Trashed"),
+                          ],
+              ),
+            )
+
+          ],
+        ),
       ),
 
 
--- lib/widgets/UiHelper.dart
+++ lib/widgets/UiHelper.dart
@@ -2,18 +2,50 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class UiHelper {
+  CustomAppBar() => Container(
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            CircleAvatar(backgroundImage: AssetImage('asset/images/img_1.png')),
+            Text(
+              "Hii,Rohit",
+              style: TextStyle(
+                  fontFamily: "Custom",
+                  color: Colors.white,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              width: 150,
+            ),
+            CircleAvatar(
+                child: Icon(
+                  Icons.menu_rounded,
+                  color: Colors.white,
+                ),
+                backgroundColor: Colors.white12),
+          ],
+        ),
+      );
 
-  CustomAppBar()=> Container(
-    width: double.infinity,
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        CircleAvatar(backgroundImage: AssetImage('asset/images/img_1.png')),
-        Text("Hii,Rohit",style: TextStyle(fontFamily: "Custom",color: Colors.white,fontSize: 22,fontWeight: FontWeight.w500),),
-        SizedBox(width: 150,),
-        CircleAvatar(child: Icon(Icons.menu_rounded,color: Colors.white,),backgroundColor: Colors.white12),
-      ],
+  CustomText() => Text(
+        "My Notes",
+        style: TextStyle(
+            fontSize: 40,
+            fontFamily: 'Custom',
+            fontWeight: FontWeight.w600,
+            color: Colors.white),
+      );
+
+  CustomFilterContainer(String text)=>Container(
+    child: Padding(
+      padding: const EdgeInsets.only(top: 5,bottom: 5,left: 11,right: 11),
+      child: Text("$text",style: TextStyle(fontFamily: 'Custom', fontSize: 16),),
+    ),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(50)
     ),
   );
-  
 }
